4a99825adc7c42cd849d423083b8cfc57448ebbf
GaryGao <garygaochinese@gmail.com>
2017-03-16 09:56:11 +0800

    github push times

--- src/main/java/com/gary/search/utils/GithubUtil.java
+++ src/main/java/com/gary/search/utils/GithubUtil.java
@@ -12,9 +12,11 @@ import java.io.InputStreamReader;
  * github util (add commit push)
  */
 public class GithubUtil {
+    private static volatile int push_times = 0 ;
     private static String git_add = " git add {fileName} ";
     private static String git_commit = " git commit -m {date} ";
-    private static String git_push = " git push -u origin master ";
+    private static String git_firstPush = " git push -u origin master ";
+    private static String git_push = " git push origin master ";
     private static Logger logger = LoggerFactory.getLogger(GithubUtil.class);
 
     public static void add_Commit_push(String date, String fileName) {
@@ -25,9 +27,16 @@ public class GithubUtil {
             Process commitExec = Runtime.getRuntime().exec(git_commit.replace("{date}", date));
             String commitResult = GithubUtil.outputResult(commitExec);
             logger.info("git commit -m " + date + " result : " + commitResult);
-            Process pushExec = Runtime.getRuntime().exec(git_push);
+            Process pushExec = null ;
+            logger.info("git push times "+String.valueOf(push_times));
+            if (push_times>0){
+                pushExec = Runtime.getRuntime().exec(git_push);
+            }else {
+                pushExec = Runtime.getRuntime().exec(git_firstPush);
+            }
             String pushResult = GithubUtil.outputResult(pushExec);
             logger.info("git push result : " + pushResult);
+            push_times+=1;
         } catch (IOException e) {
             logger.error("Github Util error", e);
         }
