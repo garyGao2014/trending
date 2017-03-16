0d01cfd040d90ee41931427fc2789c6c0459bc12
GaryGao <garygaochinese@gmail.com>
2017-03-16 09:33:28 +0800

    print github result

--- src/main/java/com/gary/search/utils/GithubUtil.java
+++ src/main/java/com/gary/search/utils/GithubUtil.java
@@ -4,6 +4,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.Charset;
 
 /**
  * Created by gaozhicheng on 2017/3/6.
@@ -17,14 +19,25 @@ public class GithubUtil {
 
     public static void add_Commit_push(String date, String fileName) {
         try {
-            Runtime.getRuntime().exec(git_add.replace("{fileName}", fileName));
-            logger.info("git add " + fileName + " success ");
-            Runtime.getRuntime().exec(git_commit.replace("{date}", date));
-            logger.info("git commit -m " + date + " success ");
-            Runtime.getRuntime().exec(git_push);
-            logger.info("git push success ");
+            Process addExec = Runtime.getRuntime().exec(git_add.replace("{fileName}", fileName));
+            String addResult = GithubUtil.outputResult(addExec);
+            logger.info("git add " + fileName + " result : " + addResult);
+            Process commitExec = Runtime.getRuntime().exec(git_commit.replace("{date}", date));
+            String commitResult = GithubUtil.outputResult(commitExec);
+            logger.info("git commit -m " + date + " result : "+commitResult);
+            Process pushExec = Runtime.getRuntime().exec(git_push);
+            String pushResult = GithubUtil.outputResult(pushExec);
+            logger.info("git push result : " + pushResult);
         } catch (IOException e) {
             logger.error("Github Util error", e);
         }
+    }
+
+    private static String outputResult(Process exec) throws IOException {
+        InputStream inputStream = exec.getInputStream();
+        byte[] b = new byte[1024];
+        inputStream.read(b);
+        String result = new String(b, Charset.forName("UTF-8"));
+        return result;
     }
 }
